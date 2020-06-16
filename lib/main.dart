319caaa176440c8f5d9f17586d5140cdf67c7ffe
lwb <18614005205@163.com>
2020-06-16 15:08:22 +0800

    Text 组件属性  展示。 Container 容器组件 属性 展示。

--- lib/main.dart
+++ lib/main.dart
@@ -30,9 +30,41 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         title: Text("主页"),
       ),
-      body: Center(
-        child: Text("Body"),
+      body: HomeContent(),
+    );
+  }
+}
+
+class HomeContent extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        child: Text(
+          "我是一个文本我是一个文本我是一个文本我是一个文本",
+          overflow: TextOverflow.ellipsis,
+          maxLines: 2,
+          textAlign: TextAlign.center,
+          textScaleFactor: 1.2,
+          style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: Colors.red,
+              fontSize: 15.0,
+              fontStyle: FontStyle.italic,
+              decoration: TextDecoration.underline,
+              letterSpacing: 9.0,
+              decorationColor: Colors.green),
+        ),
+        height: 300.0,
+        width: 300.0,
+        padding: EdgeInsets.all(10.0),
+        decoration: BoxDecoration(
+            color: Colors.yellow,
+            border: Border.all(width: 1.0, color: Colors.blue),
+            borderRadius: BorderRadius.all(Radius.circular(10.0))),
       ),
     );
   }
 }
+
+
