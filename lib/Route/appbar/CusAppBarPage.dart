aa8f289d9ccc803c9a351767942b59e52b03ef4c
lwb <18614005205@163.com>
2020-06-22 14:32:31 +0800

    自定义导航栏,以及状态栏样式，标题文字样式，action 图标样式等。

--- lib/Route/appbar/CusAppBarPage.dart
+++ lib/Route/appbar/CusAppBarPage.dart
@@ -7,10 +7,15 @@ class CustomAppbar extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.lightGreen,
+        iconTheme: IconThemeData(color: Colors.black,opacity: 0.6),     ///设置导航栏图标颜色
+        actionsIconTheme: IconThemeData(color: Colors.blue,opacity: 0.6),   ///设置导航右边图标的主题色，此时iconTheme对于右边图标颜色会失效
+        brightness: Brightness.light,  ///设置导航条上面的状态栏显示字体颜色
+        textTheme: TextTheme( ///设置AppBar上面各种字体主题色
+            title: TextStyle(color: Colors.red,fontSize: 20.0,fontWeight:FontWeight.bold),
+        ),
+        backgroundColor: Colors.lightGreen, /// 导航栏背景颜色
+        centerTitle: true,  /// 标题居中显示
         title: Text("自定义导航"),
-        leading: Icon(Icons.menu),
-        centerTitle: true,
         actions: <Widget>[
           IconButton(
             icon: Icon(Icons.search),
@@ -19,19 +24,76 @@ class CustomAppbar extends StatelessWidget {
               print("Search pressed");
             },
           ),
-          IconButton(
-            icon: Icon(Icons.more_vert),
-            tooltip: "Setting",
-            onPressed: () {},
+          PopupMenuButton(
+            tooltip: "长按提示",
+            initialValue: "hot",
+            padding: EdgeInsets.all(10.0),
+            itemBuilder: _itemBuilder,
+            onSelected: _onSelected,
           ),
         ],
       ),
       body: Center(
-        child: Text(
-          "这是自定义appbar",
-          style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
-        ),
+        child: Text("自定义导航"),
       ),
     );
+  }
+
+  /// 选中 popMenu 回掉
+  void _onSelected(String action) {
+    switch (action) {
+      case "hot":
+        print("热度");
+        break;
+      case "new":
+        print("最新");
+        break;
+      case "sao":
+        print("扫一扫");
+        break;
+    }
+  }
+
+  /// popMenu item 构建器
+  List<PopupMenuEntry<String>> _itemBuilder(BuildContext context) {
+    return <PopupMenuItem<String>>[
+      PopupMenuItem<String>(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Icon(
+              Icons.message,
+              color: Colors.blue,
+            ),
+            Text("发起群聊", style: TextStyle(color: Colors.blue)),
+          ],
+        ),
+        value: "hot",
+      ),
+      PopupMenuItem<String>(
+        child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Icon(
+                Icons.group_add,
+                color: Colors.blue,
+              ),
+              Text("添加服务", style: TextStyle(color: Colors.blue)),
+            ]),
+        value: "new",
+      ),
+      PopupMenuItem<String>(
+        child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Icon(
+                Icons.cast_connected,
+                color: Colors.blue,
+              ),
+              Text("扫一扫", style: TextStyle(color: Colors.blue)),
+            ]),
+        value: "sao",
+      ),
+    ];
   }
 }
